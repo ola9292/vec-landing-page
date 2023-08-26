8af9812f20d77de1429123fecd0d8e11fedea133
ola9292 <okaz692@gmail.com>
2023-08-26 16:41:20 +0100

    update

---
+++ test.js
@@ -0,0 +1,37 @@
+function initializeCarousel(slideshow, slidesToRoll) {
+    $('.slick-carousel').slick({
+        infinite: true,
+        slidesToShow: slideshow,
+        slidesToScroll: slidesToRoll,
+        respondTo: 'slider',
+        adaptiveHeight: true,
+        dots: true
+    });
+}
+
+function destroyCarousel() {
+    $('.slick-carousel').slick('unslick');
+}
+
+// Initial setup on document load
+initializeCarousel(4, 4);
+
+// Update on window resize
+window.addEventListener('resize', function() {
+    const screenWidth = window.innerWidth;
+    if (screenWidth <= 630) {
+        destroyCarousel();
+        initializeCarousel(1, 1);
+    } 
+    else if (screenWidth <= 1000) {
+        destroyCarousel();
+        initializeCarousel(2, 2);
+    } else if (screenWidth <= 1543) {
+        destroyCarousel();
+        initializeCarousel(3, 3);
+    } else {
+        destroyCarousel();
+        initializeCarousel(4, 4);
+    }
+});
+
